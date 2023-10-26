aae5e6bc1d750ea31b302c5bb1d0da27b3d5de51
Maria <you@example.com>
2023-10-25 21:51:05 -0400

    Destination page: Desktop screen and image of the planets

--- js/destination.js
+++ js/destination.js
@@ -1,10 +1,11 @@
 const planetTabs = document.querySelector(".tabs-ul");
+const planetImg = document.querySelector(".planet-image");
 
 const changePlanetTab = (e) => {
 
   const planetTabActive = document.querySelector(".tabs-ul a.active");
   const planetDivActive = document.querySelector(".container-info__planet__div.active");
-  
+
   if (e.target.tagName == "A") {
     planetTabActive.classList.remove("active");
     planetDivActive.classList.remove("active");
@@ -12,6 +13,8 @@ const changePlanetTab = (e) => {
     let id = e.target.href.split("#")[1];
     document.querySelector(`a[href="#${id}"]`).classList.add("active");
     document.getElementById(id).classList.add("active");
+
+    planetImg.setAttribute("src", `../assets/destination/image-${id}.png`)
   }
 };
 
