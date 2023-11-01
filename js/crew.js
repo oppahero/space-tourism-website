985c371c4efdb46e07bf13a545465040b2a3b9bf
María López <37983313+oppahero@users.noreply.github.com>
2023-11-01 12:01:05 -0400

    Crew page: Fixed issue on json import

--- js/crew.js
+++ js/crew.js
@@ -1,41 +1,45 @@
-import data from "./data.json" assert { type: 'json' };;
+//import data from "./data.json" assert { type: 'json' };;
 const personImage = document.querySelector(".person-image");
 const crewRol = document.querySelector(".container-crew__info__rol");
 const crewName = document.querySelector(".container-crew__info__name");
 const crewBio = document.querySelector(".container-crew__bio");
 
+let id = 0;
+let crew;
+
+fetch("../js/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    crew = data.crew;
+  });
 
 const changeCircleActive = () => {
-    let circleActive = document.querySelector("circle.active");
-    circleActive.setAttribute("opacity", "0.174363");
-    circleActive.classList.remove("active");
+  let circleActive = document.querySelector("circle.active");
+  circleActive.setAttribute("opacity", "0.174363");
+  circleActive.classList.remove("active");
 
-    let newCircleActive = circleActive.nextElementSibling;
+  let newCircleActive = circleActive.nextElementSibling;
 
-    if (newCircleActive) {
-        newCircleActive.setAttribute("opacity", "1");
-        newCircleActive.classList.add("active");
-    } else {
-        let circles = document.querySelector("svg");
-        let firstCircle = circles.firstElementChild;
-        firstCircle.setAttribute("opacity", "1");
-        firstCircle.classList.add("active");
-    }
-}
-
-let id = 0
+  if (newCircleActive) {
+    newCircleActive.setAttribute("opacity", "1");
+    newCircleActive.classList.add("active");
+  } else {
+    let circles = document.querySelector("svg");
+    let firstCircle = circles.firstElementChild;
+    firstCircle.setAttribute("opacity", "1");
+    firstCircle.classList.add("active");
+  }
+};
 
 setInterval(function () {
+  changeCircleActive();
 
-    changeCircleActive();
+  id++;
+  if (id === crew.length) id = 0;
 
-    id++;
-    if (id === data.crew.length) id = 0;
-
-    crewRol.innerHTML = data.crew[id].role;
-    crewName.innerHTML = data.crew[id].name;
-    crewBio.innerHTML = data.crew[id].bio;
-    personImage.setAttribute("src", data.crew[id].images.png);
-    personImage.setAttribute("id", data.crew[id].id);
+  crewRol.innerHTML = crew[id].role;
+  crewName.innerHTML = crew[id].name;
+  crewBio.innerHTML = crew[id].bio;
+  personImage.setAttribute("src", crew[id].images.png);
+  personImage.setAttribute("id", crew[id].id);
 }, 4500);
-
